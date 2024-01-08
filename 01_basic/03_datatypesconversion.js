2d3ca9037702d8c0791d575bd66fc30d4af60945
kinish123 <149240572+kinish123@users.noreply.github.com>
2024-01-08 18:41:29 +0000

    conversion of datatypes

---
+++ 01_basic/03_datatypesconversion.js
@@ -0,0 +1,24 @@
+let score="33";
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueinnumber=Number(score);
+console.log(typeof valueinnumber);
+
+//"33"=33
+//"33sdfs"=NaN (not a number)
+//true=1,false=0
+
+
+let isloggedin=1;
+let booleanisloggedin=Boolean(isloggedin);
+console.log(booleanisloggedin);
+
+//1=true 0=false
+//""=false
+//"hitesh"=true
+
+let somenumber=33;
+let stringnumber=String(number)
+console.log(stringnumber);
+console.log(typeof stringnumber);
