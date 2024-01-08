692a00d24db6fda7a065a95aad546ff8c33cf44c
kinish123 <149240572+kinish123@users.noreply.github.com>
2024-01-08 20:58:55 +0000

    memory

---
+++ 01_basic/04_memory.js
@@ -0,0 +1,24 @@
+//stack(primitive) and 
+//heap(non primitive)==arry,object,functions
+//in stack the copy of original value is changed
+//in heap the changes are made in the original values
+
+let myyoutubename="kinishneema"
+let anothername=myyoutubename
+anothername="chaiorcode"
+
+console.log(myyoutubename);
+console.log(anothername);
+
+let userone={
+    email:"kinish@gmail.com",
+    upi:"kinish@yre"
+}
+let usertwo=userone;
+
+userone.email="kinishneema@gmail.com"
+
+console.log(userone.email);
+console.log(usertwo.email);
+
+
