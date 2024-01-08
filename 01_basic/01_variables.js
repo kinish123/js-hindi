eb34d04178a6ba93c65bbbd93f4982e345e9c55b
kinish123 <149240572+kinish123@users.noreply.github.com>
2024-01-08 18:23:35 +0000

    datatypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,17 @@
+const accountid =14453
+let accountemail="kinish@123"
+var accountpassword="12345"
+acountcity="jaipur"
+let accountstate;
+
+//accountid=2// not allowed
+
+accountemail="neema@gmail.com"
+accountpassword="mvdvm"
+acountcity="pune"
+
+console.log(accountid);
+
+//prefer not to use var because issue in block scope and functional scope   
+
+console.table([accountid,accountemail,accountpassword,acountcity,accountstate]);
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as newer version
+
+let name="kinish";
+let age=16;
+let isloggedin=false
+
+//number=2 to the power 53
+//bigint
+//string
+//boolean
+//null
+//undefined
+//symbol=unique
+
+//object
+
+console.log(typeof "kinish");
+console.log(typeof null);
+console.log(typeof undefined);
