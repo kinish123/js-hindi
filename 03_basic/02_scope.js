3d4172cec072704c3a79ec1c5b3b0043516b70fa
kinish123 <149240572+kinish123@users.noreply.github.com>
2024-01-10 05:49:02 +0000

    scope

---
+++ 03_basic/02_scope.js
@@ -0,0 +1,15 @@
+let a=30
+const b=20
+var c=30
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+let d=300;
+if(true)
+{
+    let d=322;
+    console.log("inner: ",a);
+}
+console.log(d);
---
+++ 03_basic/03_scope.js
@@ -0,0 +1,28 @@
+function one(){
+    const username="kinish"
+
+    function two()
+    {
+        const website="youtube"
+        console.log(username)
+
+    }
+    //console.log(website)
+   two()
+}
+one()
+
+
+function addone(num)
+{
+    return num +1
+}
+
+addone(4)
+
+const addtwo=function(num)
+{
+    return num+2
+}
+
+addtwo(5)
