9479b1d4fe45128fc4e82e2f5d67073895601779
kinish123 <149240572+kinish123@users.noreply.github.com>
2024-01-10 19:57:33 +0000

    loops

---
+++ loops/02.js
@@ -0,0 +1,32 @@
+//while
+
+let index=0
+while (index<=10) {
+    console.log(`value of index is ${index}`);
+    index=index+2;
+}
+
+
+let myarr=["dhruv","kinish","devansh"]
+let arr=0;
+while(arr<myarr.length){
+    console.log(`value is ${myarr[arr]}`)
+    arr++;
+}
+
+
+
+//do while
+
+let score=1;
+do {
+    console.log(`score is ${score}`)
+    score++;
+} while (score<=10);
+
+
+let score1=11;
+do {
+    console.log(`score is ${score1}`)
+    score1++;
+} while (score1<=10);
