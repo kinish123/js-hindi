5add325cf6eb07e61dd3e421274e5b9a83c95d8f
kinish123 <149240572+kinish123@users.noreply.github.com>
2024-01-10 11:16:59 +0000

    scope

--- 03_basic/03_scope.js
+++ 03_basic/03_scope.js
@@ -12,17 +12,42 @@ function one(){
 }
 one()
 
+if (true){
+    const username="kinish"
+    if(username==="kinish")
+    {
+        const website=" youtube"
+        console.log(username+website);
+    }
+    //console.log(website);
+}
+//console.log(username);
+
 
 function addone(num)
 {
     return num +1
 }
-
 addone(4)
 
 const addtwo=function(num)
 {
     return num+2
 }
-
 addtwo(5)
+
+
+
+// addone(4)
+// function addone(num)
+// {
+//     return num +1
+// }
+// //this will work
+
+// addtwo(5)
+// const addtwo=function(num)
+// {
+//     return num+2
+// }
+// //this will not work
